815ceb7801319a464c850388e62226b71e4cd8ea
noel-jose <noeljosetp@gmail.com>
2021-12-13 21:17:16 +0530

    Added card component for scholarship page

--- pages/scholarships.js
+++ pages/scholarships.js
@@ -1,22 +1,20 @@
 import styles from '../styles/Home.module.css'
 import {useEffect} from 'react';
 import {getFruits} from '../utils/getFruits';
+import Card from '../components/Card';
 
 const Scholarships=()=> {
     useEffect(()=>{
       getFruits(); //check the console to see data coming from firebase
     },[])
     return (
-        <div className="hero min-h-screen bg-base-200">
-        <div className="text-center hero-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">
-                  Hello there! This is the scholarships page!
-                </h1> 
-            <p className="mb-5">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p> 
-            <button className="btn btn-primary">Get Started</button>
+        <div className="hero min-h-screen bg-white-200">
+        <div className=" hero-content">
+          <div className="max-w-3/4">
+            <h1 className="text-center mb-5 text-5xl font-bold text-blue-500">
+                  Scholarships
+            </h1>
+            <Card></Card>
           </div>
         </div>
       </div>
