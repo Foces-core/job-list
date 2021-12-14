294d8614286856e4808ec983632f0c8a5f7c08fd
noel-jose <noeljosetp@gmail.com>
2021-12-14 21:55:18 +0530

    modified card.js

--- components/Card.js
+++ components/Card.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function Card() {
+function Card({name,desc}) {
     return (
         <div className="card lg:card-side border-2 border-blue-500 text-left bg-stone-50 m-4">
             <div className="card-body">
-            <h2 className="card-title font-bold">DRDO Mechanical Fellowship 2021</h2> 
+            <h2 className="card-title font-bold">{name}</h2> 
             <p className="text-gray-500">DRDO Defence Laboratory Junior Research Fellowship in Mechanical 2021 is an opportunity offered to graduate and postgraduate degree holders. The selected fellows will receive a monthly stipend of INR 31,000 plus HRA and medical facilities as per rules.</p> 
             <div className="flex flex-row items-center justify-between mt-2">
                 <div className="card-actions">
