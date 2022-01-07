02e975e7877f27335a6a95b294dabcf021da2f7c
Rithin Samuel <rithins36@gmail.com>
2022-01-07 19:47:19 +0530

    m

--- components/hero.js
+++ components/hero.js
@@ -4,29 +4,29 @@ import Link from 'next/link'
 function Welcome(){
     return(
 <div className="hero min-h-screen bg-base-200">
-  <div className="flex-col hero-content p-0 lg:flex-row-reverse grid-cols-2 ">
-    <img src="/homepage.png" className="w-2/4 rounded-lg shadow-2xl basis-1/2" /> 
-    <div className="basis-1/2 w-2/4">
-      <h1 className="mb-5 text-5xl font-bold break-all">
+  <div className="flex-col hero-content lg:flex-row-reverse max-w-screen-2xl p-0 w-screen">
+    <img src="/homepage.png" className="rounded-lg shadow-2xl basis-1/2 w-2/4"/> 
+      <div className="basis-1/2 w-2/4">
+          <h1 className="mb-5 text-5xl font-bold ">
             Find Unique career <br></br>
             opportunities for college <br></br>
             students
           </h1> 
-      <p className="mb-5">
-      Explore latest scholarships and job <br></br> 
-      opportunities for college students.
-      </p>
+        <p className="mb-5">
+          Explore latest scholarships and job <br></br> 
+          opportunities for college students.
+        </p>
             <Link href='/scholarships'>
-                   <button id="#scholarships" className="mr-6 btn btn-primary bg-primary">Find Scholarships</button>
+                   <button id="#scholarships" className="mr-6 btn btn-primary px-8 bg-primary">Find Scholarships</button>
  
             </Link>
             <Link href='/jobs'>
-                    <button id="#jobs" className="btn btn-primary bg-primary-focus">Find FresherJobs</button>
+                    <button id="#jobs" className="btn btn-primary px-8 bg-primary-focus">Find FresherJobs</button>
 
             </Link>
-    </div>
+      </div>
   </div>
-</div> 
+</div>
 )
 }
 
