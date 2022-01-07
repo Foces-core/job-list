f70a27a3d6a0da216df33daf2af0f3ce0aaf3d46
Rithin Samuel <rithins36@gmail.com>
2022-01-07 20:13:58 +0530

    Updated hero

--- components/hero.js
+++ components/hero.js
@@ -4,9 +4,9 @@ import Link from 'next/link'
 function Welcome(){
     return(
 <div className="hero min-h-screen bg-base-200">
-  <div className="flex-col hero-content lg:flex-row-reverse max-w-screen-2xl p-0 w-screen">
-    <img src="/homepage.png" className="rounded-lg shadow-2xl basis-1/2 w-2/4"/> 
-      <div className="basis-1/2 w-2/4">
+  <div className="flex-col hero-content lg:flex-row-reverse max-w-screen-2xl p-0 w-screen max-h-min">
+    <img src="/homepage.png" className="basis-1/2 w-2/4"/> 
+      <div className="basis-1/2 w-2/4 p-24">
           <h1 className="mb-5 text-5xl font-bold ">
             Find Unique career <br></br>
             opportunities for college <br></br>
