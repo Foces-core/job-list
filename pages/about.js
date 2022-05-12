8ac6ac3d61665c014fc0c5a7627d8b8e9704bda7
Rithin Samuel <rithinsamuel12@gmail.com>
2022-05-12 07:51:23 +0530

    Updated

--- pages/about.js
+++ pages/about.js
@@ -11,18 +11,19 @@ export default function About() {
       </Head>
             <Navbar />
             {/* First Card */}
-            <div className="card card-side bg-base-100 m-20 justify-between items-center rounded-none">
-            <div className="w-1/3 ">
-              <h1 className="text-left mb-8 text-2xl font-bold text-blue-500 md:text-black md:text-4xl">About Scholarfox</h1>
+            <div className="md:card md:card-side bg-base-100 md:m-20 md:justify-between items-center rounded-none md:flex-row-reverse p-8">
+            <figure className="card rounded-none" ><img src="/hat-throwing.png" alt="Movie"/></figure>
+            <div className="md:w-1/3 md:pt-40 ">
+              <h1 className="text-left md:mb-8 text-2xl font-bold text-blue-500 md:text-black md:text-4xl pt-6 mb-6">About Scholarfox</h1>
               <p className="text-base-content">Scholarfox is here to help college students find scholarships and freshers’ job openings. Students looking for scholarships can browse through a list of relevant programs and search for scholarships of their interest.</p>
             </div>
-            <figure className="card rounded-none" ><img src="/hat-throwing.png" alt="Movie"/></figure>
+           
           </div>
 
           {/* Second Card */}
-          <div className="card card-side bg-base-100 m-20 justify-between items-center rounded-none">
+          <div className="md:card md:card-side bg-base-100 md:m-20 justify-between items-center rounded-none p-8">
           <figure className="card rounded-none" ><img src="/graduation.png" alt="Graduation Picture"/></figure>
-            <div className="w-2/4 ">
+            <div className="md:w-2/4 pt-10">
               
               <p className="text-base-content mb-8">Considering the sheer amount of scholarship opportunities available around us, this site makes it very easy to get the most out of your college education.</p>
               
@@ -32,13 +33,14 @@ export default function About() {
           </div>
 
           {/* Third Card */}
-          <div className="card card-side bg-base-100 m-20 justify-between items-center rounded-none">
-            <div className="w-1/2 ">
+          <div className="md:card md:card-side bg-base-100 md:m-20 justify-between items-center rounded-none md:flex-row-reverse p-8">
+          <figure className="card rounded-none" ><img src="/foces-logo.svg" alt="Foces Logo"/></figure>
+            <div className="md:w-1/2  ">
               <h2 className="text-left mb-8 text-2xl font-bold text-blue-500 md:text-black md:text-2xl">A Product by FOCES.web team</h2>
               <p className="text-base-content  mb-8">Forum Of Computer Engineering Students (FOCES) is the official forum of the Computer Science and Engineering students of CEC.All CSE students are compulsory members of this forum .Its the platform where students can share , implement and develop their productive ideas.</p>
               <button className="rounded-none btn btn-primary normal-case font-normal bg-blue-700"><a href="https://foces-core.github.io/Foces-Website/">Know more about FOCES</a></button>
             </div>
-            <figure className="card rounded-none" ><img src="/foces-logo.svg" alt="Foces Logo"/></figure>
+            
           </div>
           <Footer/>
         </div>
